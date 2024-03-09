607c00a4add0c44496a7ac36cb7324523c378b0a
Fabian Leiva <123709798+fabianleiva@users.noreply.github.com>
2024-03-09 19:34:29 -0300

    implement put and delete functions

--- back/src/controllers/likeMeControllers.js
+++ back/src/controllers/likeMeControllers.js
@@ -1,9 +1,14 @@
-import { getPostData, createPostData } from "../models/likeMeModel.js";
+import {
+  getPostData,
+  createPostData,
+  editPostLikes,
+  deletePostData,
+} from "../models/likeMeModel.js";
 
 const getAllPost = async (req, res) => {
   try {
     const posts = await getPostData();
-    res.status(200).json({ posts: posts });
+    res.status(200).json(posts);
   } catch (error) {
     res.status(500).json({ error: "Error al procesar la solicitud" });
     console.error("Error al procesar la solicitud:", error);
@@ -20,4 +25,26 @@ const createAllPost = async (req, res) => {
     console.error("Error al procesar la solicitud:", error);
   }
 };
-export { getAllPost, createAllPost };
+
+const addLike = async (req, res) => {
+  const { id } = req.params;
+  try {
+    await editPostLikes(id);
+    res.send("Like agregado!");
+  } catch (error) {
+    res.status(500).json({ error: "Error al procesar la solicitud" });
+    console.error("Error al procesar la solicitud:", error);
+  }
+};
+
+const removePost = async (req, res) => {
+  const { id } = req.params;
+  try {
+    await deletePostData(id);
+    res.send('Post eliminado!')
+  } catch (error) {
+    
+  }
+}
+
+export { getAllPost, createAllPost, addLike, removePost };
--- back/src/models/likeMeModel.js
+++ back/src/models/likeMeModel.js
@@ -2,25 +2,35 @@ import pool from "../../db/conectionDb.js";
 
 const getPostData = async () => {
   const SQLquery = { text: "SELECT * FROM posts" };
-  try {
-    const response = await pool.query(SQLquery);
-    return response.rows;
-  } catch (error) {
-    console.log(error);
-  }
+  const response = await pool.query(SQLquery);
+  return response.rows;
 };
 
 const createPostData = async ({ titulo, img, descripcion }) => {
   const SQLquery = {
-    text: "INSERT INTO posts (titulo, img, descripcion) VALUES ($1, $2, $3) RETURNING *",
-    values: [titulo, img, descripcion],
+    text: "INSERT INTO posts (titulo, img, descripcion, likes) VALUES ($1, $2, $3, $4) RETURNING *",
+    values: [titulo, img, descripcion, 0],
   };
-  try {
-    const response = await pool.query(SQLquery);
-    return response.rows;
-  } catch (error) {
-    console.log(error);
-  }
+  const response = await pool.query(SQLquery);
+  return response.rows;
 };
 
-export { getPostData, createPostData };
+const editPostLikes = async (id) => {
+  const SQLquery = {
+    text: "UPDATE posts SET likes = likes + 1 WHERE id = $1 RETURNING *",
+    values: [id],
+  };
+  const response = await pool.query(SQLquery);
+  return response.rows;
+};
+
+const deletePostData = async (id) => {
+  const SQLquery = {
+    text: "DELETE FROM posts WHERE id = $1",
+    values: [id],
+  };
+  const response = await pool.query(SQLquery);
+  return response.rows;
+};
+
+export { getPostData, createPostData, editPostLikes, deletePostData };
