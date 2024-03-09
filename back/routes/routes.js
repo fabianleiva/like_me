4bf1d12300bf4fb96c11b4e521f9c0148d137b2a
Fabian Leiva <123709798+fabianleiva@users.noreply.github.com>
2024-03-09 19:33:57 -0300

    create put and delete routes

--- back/routes/routes.js
+++ back/routes/routes.js
@@ -1,9 +1,15 @@
-import express from 'express';
-import {getAllPost, createAllPost} from '../src/controllers/likeMeControllers.js';
+import express from "express";
+import {
+  getAllPost,
+  createAllPost,
+  addLike,
+  removePost,
+} from "../src/controllers/likeMeControllers.js";
 const router = express.Router();
 
-
-router.get('/posts', getAllPost);
-router.post('/posts', createAllPost);
+router.get("/posts", getAllPost);
+router.post("/posts", createAllPost);
+router.put("/posts/like/:id", addLike);
+router.delete("/posts/:id", removePost);
 
 export default router;
