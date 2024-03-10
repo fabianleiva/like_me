ec1c75ac2d4d7a9c222f638074da31975dbd2c05
Fabian Leiva <123709798+fabianleiva@users.noreply.github.com>
2024-03-10 11:40:03 -0300

    improve error management

--- back/src/controllers/likeMeControllers.js
+++ back/src/controllers/likeMeControllers.js
@@ -10,8 +10,8 @@ const getAllPost = async (req, res) => {
     const posts = await getPostData();
     res.status(200).json(posts);
   } catch (error) {
-    res.status(500).json({ error: "Error al procesar la solicitud" });
-    console.error("Error al procesar la solicitud:", error);
+    res.status(500).json({ error: "No se ha podido procesar la solicitud" });
+    console.error("No se ha podido procesar la solicitud:", error);
   }
 };
 
@@ -21,30 +21,37 @@ const createAllPost = async (req, res) => {
     const newPost = await createPostData(post);
     res.status(201).json({ post: newPost });
   } catch (error) {
-    res.status(500).json({ error: "Error al procesar la solicitud" });
-    console.error("Error al procesar la solicitud:", error);
+    res.status(400).json({
+      error: "No se ha podido procesar la solicitud, post no agregado",
+    });
+    console.error("No se ha podido procesar la solicitud:", error);
   }
 };
 
 const addLike = async (req, res) => {
-  const { id } = req.params;
   try {
-    await editPostLikes(id);
-    res.send("Like agregado!");
+    const { id } = req.params;
+    const post = await editPostLikes(id);
+    res.status(200).json("Like agregado");
   } catch (error) {
-    res.status(500).json({ error: "Error al procesar la solicitud" });
-    console.error("Error al procesar la solicitud:", error);
+    res.status(500).json({
+      error: "No se ha podido procesar la solicitud, like no agregado",
+    });
+    console.error("No se ha podido procesar la solicitud:", error);
   }
 };
 
 const removePost = async (req, res) => {
-  const { id } = req.params;
   try {
-    await deletePostData(id);
-    res.send('Post eliminado!')
+    const { id } = req.params;
+    const deletePost = await deletePostData(id);
+    res.status(200).json("Post eliminado");
   } catch (error) {
-    
+    res.status(500).json({
+      error: "No se ha podido procesar la solicitud, post no encontrado",
+    });
+    console.error("No se ha podido procesar la solicitud:", error);
   }
-}
+};
 
 export { getAllPost, createAllPost, addLike, removePost };
